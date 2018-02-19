3428a114028fb2602f3f0fec81e3b8601d101437
Thibault <kervarecht@gmail.com>
2018-02-19 16:34:24 +0000

    Passport and Routes setup

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //reqs
 var express = require('express');
+var app = express();
 var mongo = require('mongodb').MongoClient;
 var session = require('express-session');  //need to learn more about how this works
 var cookieParser = require('cookie-parser');
 require('dotenv').config();
+var bodyParser = require("body-parser");
 
 //passport reqs
 var passport = require('passport');
@@ -12,11 +14,55 @@ var TwitterStrategy = require('passport-twitter');
 var FacebookStrategy = require('passport-facebook');
 var GoogleStrategy = require('passport-google');
 
-//handlebars
+//express handlebars
 var exphbs = require('express-handlebars');
+var hbs = exphbs.create({defaultLayout: 'main'});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+
+//=========STATIC FILES=========//
+app.use(express.static(__dirname + "/auth"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 //==========EXPRESS=============//
-var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//=========PASSPORT===========//
+var funcs = require('./auth/logic/log-ops.js'); //import funcs.localLogin and funcs.LocalSignup
+var auth = require('./auth/logic/auth.js');  //import session verification
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
 
 //===========ROUTES==============//
+app.get('/', function(req, res){
+   res.render("index", {user: req.username}); 
+});
+//logging in local user
+app.get('/login', function(req, res){
+   res.render("login"); 
+});
+
+app.post('/login-user', function(req, res){
+   var request = {
+      'username' : req.body.username,
+      'password' : req.body.password
+   };
+   
+   funcs.localLogin(req.body.username, req.body.password);
+   //username is user1, password is hash for MongoDB test login
+   
+});
+
+//registering local user
+app.get('/signup', function(req, res){
+   res.render('signup');
+});
+
+//==========PORT==============//
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log("listening on " + port + "!");
--- auth/logic/log-ops.js
+++ auth/logic/log-ops.js
@@ -1,1 +1,90 @@
+//Requirements
+var mongo = require('mongodb').MongoClient;
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+
+//database
+var url = process.env.MONGO_DATABASE
 //This file will contain the mongo operations for Local Login and Sign Up
+
+
+/*Two Functions
+Login - Takes username and password, checks:
+Does username exist?  If no return login fail
+Is password right?  Hash via bcryp and compareSync, if yes return login successful and user object
+
+Signup - takes username and password, checks:
+Does username exist?  If yes, return signup fail
+Store username and password hash via bcrypt 
+*/
+
+exports.localLogin = function(username, password){
+    var deferred = Q.defer();
+    
+    mongo.connect(url, function(err, db){
+        if (err) throw err;
+        
+        var collection = db.collection('user-info');
+        
+        collection.findOne({'username': username})
+        .then(function(result){
+            console.log(result);
+           if (null == result){
+               console.log("Username not found")
+               deferred.resolve(false); //return false boolean for promise
+           } 
+           else {
+               var hash = result.password //existing password for the profile
+               console.log("Username found.")
+               
+               if (bcrypt.compareSync(password, hash)){
+                   deferred.resolve(result) //send the database user object to return to user
+                    console.log("User logged in!");
+                  
+               }
+               else {
+                   console.log("Authentication failed");
+                   deferred.resolve(false) //return false boolean for promise
+               }
+           }
+        });
+        db.close();
+    });
+    return deferred.promise; //return promise either with user object or with false value for login failed
+}
+
+exports.localSignup = function(username, password){
+    var deferred = Q.defer();
+    
+    mongo.connect(url, function(err, db){
+        if (err) throw err;
+        
+        var collection = db.collection('user-info');
+        
+        collection.findOne({'username': username})
+        .then(function(result){
+            if (result !== null){
+                //username already exists
+                console.log("Username already exists");
+                deferred.resolve(false);
+            }
+            else {
+                //hash password
+                var hash = bcrypt.hashSync(password, 8);
+                
+                var user = {
+                    'username': username,
+                    'password': hash
+                }
+                
+                collection.insert(user)
+                .then(function(){
+                    console.log("User created!");
+                    db.close();
+                    deferred.resolve(user);
+                })
+            }
+        })
+    })
+    return deferred.promise;
+}
