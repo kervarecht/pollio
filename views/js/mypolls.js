d97c2a4c5890651657b9abb03fcd7656b1bb77e9
Thibault <kervarecht@gmail.com>
2018-03-20 03:26:03 +0000

    Added aggregation option for user's poll page

--- views/js/mypolls.js
+++ views/js/mypolls.js
@@ -10,6 +10,44 @@ $(document).ready(function(){
                createChart(poll, $('.ct-chart').get(counter));
                counter++;
             });
+            
+            var totals = createAggregateStats(polls);
+            
+            $("#stats-div").append(presentStats(totals));
                 
     });
 });
+
+function createAggregateStats(array){
+    var totalPolls = 0;
+    var totalVotes = 0;
+    var totalOptions = 0;
+    
+    array.forEach(function(poll){
+        totalPolls++;
+        for (key in poll.options){
+          totalOptions++;
+          totalVotes += poll.options[key];
+        };
+    });
+    
+    var aggregateObj = {
+        'polls': totalPolls,
+        'votes': totalVotes,
+        'options': totalOptions
+    };
+    
+    return aggregateObj;
+    
+};
+
+function presentStats(object){
+    var polls = object.polls;
+    var votes = object.votes;
+    var options = object.options;
+    var pollP = '<p class="poll-stats">Total Polls: ' + polls + "</p>";
+    var optionsP = '<p class="poll-stats">Total Options: ' + options + "</p>";
+    var votesP = '<p class="poll-stats">Total Votes: ' + votes + "</p>";
+    
+    return pollP + optionsP + votesP;
+}
